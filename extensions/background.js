cde0c7fd2887dd5f46ba0148911ab64b0072c8ec
cti1650 <cti1650@gmail.com>
2022-04-19 22:26:17 +0900

    feat: 切り替え機能無効化

--- extensions/background.js
+++ extensions/background.js
@@ -1,1 +1,16 @@
+chrome.action.onClicked.addListener(function (tab) {
+  chrome.storage.local.get(["yvfe_filter"], function (items) {
+    chrome.storage.local.set({ yvfe_filter: !items.yvfe_filter });
+  });
+});
 
+// chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+//   if (message === "yvfe_filter") {
+//     await new Promise((resolve, reject) => {
+//       chrome.storage.local.get("yvfe_filter", function (items) {
+//         resolve(sendResponse(items.yvfe_filter));
+//       });
+//       reject(sendResponse(null))
+//     });
+//   }
+// });
--- extensions/js/script.js
+++ extensions/js/script.js
@@ -1,10 +1,12 @@
 console.log("script done");
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   setInterval(() => {
-    document.querySelectorAll("#playlist-items")?.forEach((item) => {
-      if (item.querySelector("div#progress")?.style.width === "100%") {
-        item.style.display = "none";
-      }
-    });
+    // chrome.runtime.sendMessage('yvfe_filter', (response) => {
+      document.querySelectorAll("#playlist-items")?.forEach((item) => {
+        if (item.querySelector("div#progress")?.style.width === "100%") {
+          item.style.display = true ? "none" : null;
+        }
+      });
+    // });
   }, 500);
-})
+});
